b4f3a824e6af83c932e88363723476d5793ea7e2
hyavgin <halidavgin@gmail.com>
2022-06-02 09:30:42 -0500

    new request submit done

--- src/main/java/com/project1/MainDriver.java
+++ src/main/java/com/project1/MainDriver.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 
 import com.project1.dao.ReimbursmentDao;
 import com.project1.dao.ReimbursmentDaoImpl;
+import com.project1.models.Reimbursment;
 import com.project1.util.ConnectionFactory;
 
 public class MainDriver {
@@ -13,7 +14,7 @@ public class MainDriver {
 		Connection connection = ConnectionFactory.getConnection();
 		ReimbursmentDao reimDao = new ReimbursmentDaoImpl();
 		reimDao.allReimbursmentById();
-		reimDao.createNewReimbursment(null);
+		reimDao.createNewReimbursment(new Reimbursment(-1,"Engineering" , "havgin", "06/02/2022", 999.99, "Lodging", "Credit", null, "Education Travil hotel expenses"));
 		reimDao.employeLogin(null);
 		
 		
--- src/main/java/com/project1/dao/ReimbursmentDaoImpl.java
+++ src/main/java/com/project1/dao/ReimbursmentDaoImpl.java
@@ -1,16 +1,39 @@
 package com.project1.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.project1.models.Reimbursment;
 import com.project1.models.User;
+import com.project1.util.ConnectionFactory;
 
 public class ReimbursmentDaoImpl implements ReimbursmentDao {
 
 	@Override
 	public void createNewReimbursment(Reimbursment r) {
-		System.out.println("calisiyormu");
-		
+		String sql = "INSERT INTO \"Project1\".Reimbursement (Department,username,Reimbursment_date,Tota_Cost, Expense_Type,Payment_Type,Description, reimuserid) values (?, ?, ?, ?, ?, ?, ?,(SELECT userid FROM \"Project1\".employee where username = ?));";
+		Connection connection = ConnectionFactory.getConnection();
+		try(PreparedStatement ps = connection.prepareStatement(sql)) {
+			ps.setString(1,r.getDepartment());
+			ps.setString(2,r.getUsername());
+			ps.setString(3,r.getDate());
+			ps.setDouble(4,r.getTotalCost());
+			ps.setString(5,r.getExpenseType());
+			ps.setString(6,r.getPaymentType());
+			ps.setString(7,r.getDescription());
+			ps.setString(8,r.getUsername());
+			
+			
+			
+			
+			ps.execute();
+			System.out.println("New request submitted");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
