301a84c779c5fa49317399d52ef60a5e86cf8f15
hyavgin <halidavgin@gmail.com>
2022-06-06 14:41:32 -0500

    Prometheus Micrometer done and counter added for employee login request submit and reimbursement

--- src/main/java/com/project1/MainDriver.java
+++ src/main/java/com/project1/MainDriver.java
@@ -1,23 +1,42 @@
 package com.project1;
 
-import java.rmi.registry.Registry;
+import java.io.File;
 
 import com.project1.controller.RequestMapper;
 
-
 import io.javalin.Javalin;
 import io.javalin.plugin.metrics.MicrometerPlugin;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.system.DiskSpaceMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 
 public class MainDriver {
+	
+	
+	static int total=0;	
 	
 	public static void main(String[] args ) {
 		
 		PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
 		registry.config().commonTags("application","My-Frist-Monitored-App");
 		
+		new ClassLoaderMetrics().bindTo(registry);
+		new JvmMemoryMetrics().bindTo(registry);
+		new JvmGcMetrics().bindTo(registry);
+		new JvmThreadMetrics().bindTo(registry);
+		new UptimeMetrics().bindTo(registry);
+		new ProcessorMetrics().bindTo(registry);
+		new DiskSpaceMetrics(new File(System.getProperty("user.dir"))).bindTo(registry);
 		
+		Counter counter = Counter.builder("Path_request").description("track number").tag("purpose", "demo").register(registry);
+
 		Javalin app = Javalin.create(config -> {config.registerPlugin(new MicrometerPlugin(registry));}).start(7115);
 		
 		
@@ -26,10 +45,6 @@ public class MainDriver {
 		
 
 
-
-		
-		
-		
 	}
 	
 	
--- src/main/java/com/project1/controller/RequestMapper.java
+++ src/main/java/com/project1/controller/RequestMapper.java
@@ -1,7 +1,19 @@
 package com.project1.controller;
 
+import java.io.File;
+
 import io.javalin.Javalin;
 import io.javalin.http.HttpCode;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.system.DiskSpaceMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 
 public class RequestMapper {
 	
@@ -11,12 +23,39 @@ public class RequestMapper {
 	public void configureRoutes(Javalin app) {
 		
 		
+		PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+		registry.config().commonTags("application","My-Frist-Monitored-App");
+		
+		new ClassLoaderMetrics().bindTo(registry);
+		new JvmMemoryMetrics().bindTo(registry);
+		new JvmGcMetrics().bindTo(registry);
+		new JvmThreadMetrics().bindTo(registry);
+		new UptimeMetrics().bindTo(registry);
+		new ProcessorMetrics().bindTo(registry);
+		new DiskSpaceMetrics(new File(System.getProperty("user.dir"))).bindTo(registry);
+		
+		Counter counter = Counter.builder("Path_request").description("track number").tag("purpose", "request_submit").register(registry);
+		Counter counter1 = Counter.builder("Path_request").description("track number").tag("purpose", "reimbursment").register(registry);
+		Counter counter2 = Counter.builder("Path_request").description("track number").tag("purpose", "login").register(registry);
+		Counter counter3 = Counter.builder("Path_request").description("track number").tag("purpose", "reimbursement_check").register(registry);
+		
+		
+
+		app.get("/metrics", ctx->{
+			ctx.result(registry.scrape());
+			
+		});
+		
+		
+		
+		
 		app.post("/api/requestSubmit", ctx ->{
 
 			boolean access = AuthenticateController.sessionCheck(ctx);
 	
 			if(access) {
-				rController.requestSubmit(ctx);				
+				rController.requestSubmit(ctx);	
+				counter.increment(1);
 			}else {
 				ctx.status(HttpCode.FORBIDDEN);
 			}
@@ -32,6 +71,7 @@ public class RequestMapper {
 			
 			if(access) {
 				rController.allReimbursment(ctx);
+				counter1.increment(1);
 			}else {
 				ctx.status(HttpCode.FORBIDDEN);
 			}	
@@ -40,6 +80,7 @@ public class RequestMapper {
 		app.post("/login", ctx->{
 			
 			AuthenticateController.authenticate(ctx);
+			counter2.increment(1);
 
 		});
 		app.get("/logout", ctx->{
@@ -75,6 +116,7 @@ public class RequestMapper {
 			
 			if(access) {
 				rController.usernameCheck(ctx);
+				counter3.increment(1);
 			}else {
 				ctx.status(HttpCode.FORBIDDEN);
 			}
@@ -164,6 +206,10 @@ public class RequestMapper {
 			
 			
 		});
+		
+
+		
+		
 		
 		
 		
