3961440d523ddee4e6a351afd560a1d9a7ffab8c
hyavgin <halidavgin@gmail.com>
2022-06-06 09:48:36 -0500

    Config debug solved employe status rearranged

--- src/main/java/com/project1/MainDriver.java
+++ src/main/java/com/project1/MainDriver.java
@@ -1,25 +1,35 @@
 package com.project1;
 
-import java.sql.Connection;
+import java.rmi.registry.Registry;
 
 import com.project1.controller.RequestMapper;
-import com.project1.dao.ReimbursmentDao;
-import com.project1.dao.ReimbursmentDaoImpl;
-import com.project1.models.Reimbursment;
-import com.project1.util.ConnectionFactory;
+
 
 import io.javalin.Javalin;
+import io.javalin.plugin.metrics.MicrometerPlugin;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 
 public class MainDriver {
 	
 	public static void main(String[] args ) {
 		
-		Javalin app = Javalin.create().start(7112);
+		PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+		registry.config().commonTags("application","My-Frist-Monitored-App");
+		
+		
+		Javalin app = Javalin.create(config -> {config.registerPlugin(new MicrometerPlugin(registry));}).start(7115);
+		
 		
 		RequestMapper requestMapper = new RequestMapper();
 		requestMapper.configureRoutes(app);
+		
 
 
+
+		
+		
+		
 	}
 	
 	
